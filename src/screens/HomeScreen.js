8daeba3d7785c51bf0f5f737235bb09a23d45496
Đặng Nguyễn Minh An <dangnguyenminhan123@gmail.com>
2023-07-17 16:12:08 +0700

    back into old code with homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,55 +1,10 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/authContext";
-import {
-  View,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Alert,
-  Image,
-} from "react-native";
+import { View, SafeAreaView, StyleSheet, Text } from "react-native";
 import COLORS from "../constants/colors";
-import * as ImagePicker from "expo-image-picker";
-import { firebase } from "../config/firebaseConfig";
 
 const Home = () => {
   const { accessToken, userDecode } = useContext(AuthContext);
-
-  const [image, setImage] = useState(null);
-  const [uploading, setUploading] = useState(false);
-
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    const source = { uri: result.uri };
-    console.log(source);
-    setImage(source);
-  };
-
-  const uploadImage = async () => {
-    setUploading(true);
-    const response = await fetch(image.uri);
-    const blob = await response.blob();
-    const filename = image.uri.substring(image.uri.lastIndexOf("/") + 1);
-    var ref = firebase.storage().ref().child(filename).put(blob);
-
-    try {
-      await ref;
-    } catch (e) {
-      console.log(e);
-    }
-
-    setUploading(false);
-    Alert.alert("Photo Uploaded..!!");
-    setImage(null);
-  };
-
   return (
     <SafeAreaView
       style={{ flex: 1, paddingHorizontal: 20, backgroundColor: COLORS.white }}
@@ -57,13 +12,11 @@ const Home = () => {
       <View style={style.home_container}>
         {!userDecode ? (
           <View style={style.header}>
-            <Text>Chưa login</Text>
+             <Text>Chưa login</Text>
           </View>
         ) : (
           <View style={style.header}>
-            <TouchableOpacity style={style.selectButton} onPress={pickImage}>
-              <Text>Pick an image</Text>
-            </TouchableOpacity>
+            <Text>{userDecode.fullName}</Text>
           </View>
         )}
       </View>
